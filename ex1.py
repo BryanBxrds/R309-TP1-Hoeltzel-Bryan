1ee214c797f3e5ea46c474b47c3837f54f762c22
BryanBxrds <hoeltzelbryan@gmail.com>
2025-10-06 16:42:11 +0200

    Ajout de reine.png

--- ex1.py
+++ ex1.py
@@ -7,23 +7,22 @@ root.title("Compteur")
 compteur = tk.IntVar(value=0) #tk.IntVar est une variable pour les entiers
 
 label = tk.Label(root, textvariable=compteur, font=("Arial", 20)) # Les labels sont des sortes de fenetres
-label.pack(pady=100) # pack pour afficher et pady pour la taille (padding)
+label.pack(pady=100) # pack pour afficher et pady pour la taille verticale (padding)
 
-# Fonction pour incrémenter
+# Fonction +
 def incrementer():
-    compteur.set(compteur.get() + 1)
+    compteur.set(compteur.get() + 1)   #compeuteur.set pour definir la valeur et compteur.get pour modifier la valeur
 
-# Fonction pour décrémenter
+# Fonction -
 def decrementer():
     compteur.set(compteur.get() - 1)
 
-# Bouton "+"
-btn_plus = tk.Button(root, text="+", command=incrementer, width=10)
+# Bouton +
+btn_plus = tk.Button(root, text="+", command=incrementer, width=10) #tk.Button pour creer un bouton
 btn_plus.pack(pady=5)
 
-# Bouton "-"
+# Bouton -
 btn_moins = tk.Button(root, text="-", command=decrementer, width=10)
 btn_moins.pack(pady=5)
 
-# Boucle principale
 root.mainloop()
